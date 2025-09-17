726f46ee349b099be94a8760ab219c36e54540b0
Bre4dGC <glebbreadc@gmail.com>
2025-09-17 21:29:46 +0300

    [refactor] enum categories definition for code readability

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -9,55 +9,63 @@
 #define DEBUG
 #include "debug.h"
 
+#define C_OP (CATEGORY_OPERATOR)
+#define C_KW (CATEGORY_KEYWORD)
+#define C_DT (CATEGORY_DATATYPE)
+#define C_MD (CATEGORY_MODIFIER)
+#define C_SV (CATEGORY_SERVICE)
+#define C_LT (CATEGORY_LITERAL)
+#define C_PR (CATEGORY_PAREN)
+#define C_DL (CATEGORY_DELIMITER)
 
 const struct keyword operators[] = {
-    {"++", OPER_INCREM, CATEGORY_OPERATOR}, {"--", OPER_DECREM,  CATEGORY_OPERATOR},
-    {"==", OPER_EQ,     CATEGORY_OPERATOR}, {"!=", OPER_NEQ,     CATEGORY_OPERATOR},
-    {"+=", OPER_ADD,    CATEGORY_OPERATOR}, {"-=", OPER_SUB,     CATEGORY_OPERATOR},
-    {"*=", OPER_MUL,    CATEGORY_OPERATOR}, {"/=", OPER_DIV,     CATEGORY_OPERATOR},
-    {"&&", OPER_AND,    CATEGORY_OPERATOR}, {"||", OPER_OR,      CATEGORY_OPERATOR},
-    {"<=", OPER_LTE,    CATEGORY_OPERATOR}, {">=", OPER_GTE,     CATEGORY_OPERATOR},
-    {"..", OPER_RANGE,  CATEGORY_OPERATOR}, {"%=", OPER_MOD,     CATEGORY_OPERATOR},
-    {"->", OPER_ARROW,  CATEGORY_OPERATOR}
+    {"++", OPER_INCREM, C_OP}, {"--", OPER_DECREM,  C_OP},
+    {"==", OPER_EQ,     C_OP}, {"!=", OPER_NEQ,     C_OP},
+    {"+=", OPER_ADD,    C_OP}, {"-=", OPER_SUB,     C_OP},
+    {"*=", OPER_MUL,    C_OP}, {"/=", OPER_DIV,     C_OP},
+    {"&&", OPER_AND,    C_OP}, {"||", OPER_OR,      C_OP},
+    {"<=", OPER_LTE,    C_OP}, {">=", OPER_GTE,     C_OP},
+    {"..", OPER_RANGE,  C_OP}, {"%=", OPER_MOD,     C_OP},
+    {"->", OPER_ARROW,  C_OP}
 };
 
 const struct keyword keywords[] = {
     /* сontrol structures */
-    {"if",      KW_IF,      CATEGORY_KEYWORD}, {"else",     KW_ELSE,      CATEGORY_KEYWORD},
-    {"elif",    KW_ELIF,    CATEGORY_KEYWORD}, {"for",      KW_FOR,       CATEGORY_KEYWORD},
-    {"do",      KW_DO,      CATEGORY_KEYWORD}, {"while",    KW_WHILE,     CATEGORY_KEYWORD},
-    {"func",    KW_FUNC,    CATEGORY_KEYWORD}, {"return",   KW_RETURN,    CATEGORY_KEYWORD},
-    {"break",   KW_BREAK,   CATEGORY_KEYWORD}, {"continue", KW_CONTINUE,  CATEGORY_KEYWORD},
-    {"default", KW_DEFAULT, CATEGORY_KEYWORD}, {"match",    KW_MATCH,     CATEGORY_KEYWORD},
-    {"case",    KW_CASE,    CATEGORY_KEYWORD}, {"struct",   KW_STRUCT,    CATEGORY_KEYWORD},
-    {"enum",    KW_ENUM,    CATEGORY_KEYWORD}, {"union",    KW_UNION,     CATEGORY_KEYWORD},
-    {"import",  KW_IMPORT,  CATEGORY_KEYWORD}, {"type",     KW_TYPE,      CATEGORY_KEYWORD},
-    {"trait",   KW_TRAIT,   CATEGORY_KEYWORD}, {"try",      KW_TRY,       CATEGORY_KEYWORD},
-    {"catch",   KW_CATCH,   CATEGORY_KEYWORD}, {"async",    KW_ASYNC,     CATEGORY_KEYWORD},
-    {"await",   KW_AWAIT,   CATEGORY_KEYWORD}, {"test",     KW_TEST,      CATEGORY_KEYWORD},
-    {"assert",  KW_ASSERT,  CATEGORY_KEYWORD}, {"verify",   KW_VERIFY,    CATEGORY_KEYWORD},
-    {"solve",   KW_SOLVE,   CATEGORY_KEYWORD}, {"where",    KW_WHERE,     CATEGORY_KEYWORD},    
-    {"snapshot",KW_SNAPSHOT,CATEGORY_KEYWORD}, {"rollback", KW_ROLLBACK,  CATEGORY_KEYWORD},
-    {"commit",  KW_COMMIT,  CATEGORY_KEYWORD}, {"fork",     KW_FORK,      CATEGORY_KEYWORD},
-    {"merge",   KW_MERGE,   CATEGORY_KEYWORD}, {"revert",   KW_REVERT,    CATEGORY_KEYWORD},
-    {"push",    KW_PUSH,    CATEGORY_KEYWORD}, {"pull",     KW_PULL,      CATEGORY_KEYWORD},
-    {"clone",   KW_CLONE,   CATEGORY_KEYWORD}, {"simulate", KW_SIMULATE,  CATEGORY_KEYWORD},
-    {"choose",  KW_CHOOSE,  CATEGORY_KEYWORD}, {"scenarios",KW_SCENARIOS, CATEGORY_KEYWORD},  
-    {"branch",  KW_BRANCH,  CATEGORY_KEYWORD},  
+    {"if",      KW_IF,      C_KW}, {"else",     KW_ELSE,      C_KW},
+    {"elif",    KW_ELIF,    C_KW}, {"for",      KW_FOR,       C_KW},
+    {"do",      KW_DO,      C_KW}, {"while",    KW_WHILE,     C_KW},
+    {"func",    KW_FUNC,    C_KW}, {"return",   KW_RETURN,    C_KW},
+    {"break",   KW_BREAK,   C_KW}, {"continue", KW_CONTINUE,  C_KW},
+    {"default", KW_DEFAULT, C_KW}, {"match",    KW_MATCH,     C_KW},
+    {"case",    KW_CASE,    C_KW}, {"struct",   KW_STRUCT,    C_KW},
+    {"enum",    KW_ENUM,    C_KW}, {"union",    KW_UNION,     C_KW},
+    {"import",  KW_IMPORT,  C_KW}, {"type",     KW_TYPE,      C_KW},
+    {"trait",   KW_TRAIT,   C_KW}, {"try",      KW_TRY,       C_KW},
+    {"catch",   KW_CATCH,   C_KW}, {"async",    KW_ASYNC,     C_KW},
+    {"await",   KW_AWAIT,   C_KW}, {"test",     KW_TEST,      C_KW},
+    {"assert",  KW_ASSERT,  C_KW}, {"verify",   KW_VERIFY,    C_KW},
+    {"solve",   KW_SOLVE,   C_KW}, {"where",    KW_WHERE,     C_KW},    
+    {"snapshot",KW_SNAPSHOT,C_KW}, {"rollback", KW_ROLLBACK,  C_KW},
+    {"commit",  KW_COMMIT,  C_KW}, {"fork",     KW_FORK,      C_KW},
+    {"merge",   KW_MERGE,   C_KW}, {"revert",   KW_REVERT,    C_KW},
+    {"push",    KW_PUSH,    C_KW}, {"pull",     KW_PULL,      C_KW},
+    {"clone",   KW_CLONE,   C_KW}, {"simulate", KW_SIMULATE,  C_KW},
+    {"choose",  KW_CHOOSE,  C_KW}, {"scenarios",KW_SCENARIOS, C_KW},  
+    {"branch",  KW_BRANCH,  C_KW},  
 
     /* data types */
-    {"int",     DT_INT,    CATEGORY_DATATYPE}, {"uint",    DT_UINT,    CATEGORY_DATATYPE},
-    {"float",   DT_FLOAT,  CATEGORY_DATATYPE}, {"str",     DT_STR,     CATEGORY_DATATYPE},
-    {"bool",    DT_BOOL,   CATEGORY_DATATYPE}, {"void",    DT_VOID,    CATEGORY_DATATYPE},
-    {"int8",    DT_INT8,   CATEGORY_DATATYPE}, {"int16",   DT_INT16,   CATEGORY_DATATYPE},
-    {"int32",   DT_INT32,  CATEGORY_DATATYPE}, {"int64",   DT_INT64,   CATEGORY_DATATYPE},
-    {"uint8",   DT_UINT8,  CATEGORY_DATATYPE}, {"uint16",  DT_UINT16,  CATEGORY_DATATYPE},
-    {"uint32",  DT_UINT32, CATEGORY_DATATYPE}, {"uint64",  DT_UINT64,  CATEGORY_DATATYPE},
-    {"float32", DT_FLOAT32,CATEGORY_DATATYPE}, {"float64", DT_FLOAT64, CATEGORY_DATATYPE},
+    {"int",     DT_INT,    C_DT}, {"uint",    DT_UINT,    C_DT},
+    {"float",   DT_FLOAT,  C_DT}, {"str",     DT_STR,     C_DT},
+    {"bool",    DT_BOOL,   C_DT}, {"void",    DT_VOID,    C_DT},
+    // {"int8",    DT_INT8,   C_DT}, {"int16",   DT_INT16,   C_DT},
+    // {"int32",   DT_INT32,  C_DT}, {"int64",   DT_INT64,   C_DT},
+    // {"uint8",   DT_UINT8,  C_DT}, {"uint16",  DT_UINT16,  C_DT},
+    // {"uint32",  DT_UINT32, C_DT}, {"uint64",  DT_UINT64,  C_DT},
+    // {"float32", DT_FLOAT32,C_DT}, {"float64", DT_FLOAT64, C_DT},
     
     /* modifiers */
-    {"var",   MOD_VAR,   CATEGORY_MODIFIER}, {"const",  MOD_CONST,  CATEGORY_MODIFIER},
-    {"final", MOD_FINAL, CATEGORY_MODIFIER}, {"static", MOD_STATIC, CATEGORY_MODIFIER},
+    {"var",   MOD_VAR,   C_MD}, {"const",  MOD_CONST,  C_MD},
+    {"final", MOD_FINAL, C_MD}, {"static", MOD_STATIC, C_MD},
 };
 
 const size_t operators_count = sizeof(operators) / sizeof(struct keyword);
@@ -70,21 +78,21 @@ struct token new_token(const enum category_tag category, const int type, const c
     if (literal){
         tok.literal = strdup(literal);
         if (!tok.literal){
-            tok.category = CATEGORY_SERVICE;
+            tok.category = C_SV;
             tok.type_service = SERV_ILLEGAL;
             return tok;
         }
     }
 
     switch (category){
-        case CATEGORY_SERVICE:    tok.type_service =    (enum category_service)type; break;
-        case CATEGORY_OPERATOR:   tok.type_operator =   (enum category_operator)type; break;
-        case CATEGORY_KEYWORD:    tok.type_keyword =    (enum category_keyword)type; break;
-        case CATEGORY_PAREN:      tok.type_paren =      (enum category_paren)type; break;
-        case CATEGORY_DELIMITER:  tok.type_delim =      (enum category_delimiter)type; break;
-        case CATEGORY_DATATYPE:   tok.type_datatype =   (enum category_datatype)type; break;
-        case CATEGORY_LITERAL:    tok.type_literal =    (enum category_literal)type; break;
-        case CATEGORY_MODIFIER:   tok.type_modifier =   (enum category_modifier)type; break;
+        case C_SV: tok.type_service =  (enum category_service)type; break;
+        case C_OP: tok.type_operator = (enum category_operator)type; break;
+        case C_KW: tok.type_keyword =  (enum category_keyword)type; break;
+        case C_PR: tok.type_paren =    (enum category_paren)type; break;
+        case C_DL: tok.type_delim =    (enum category_delimiter)type; break;
+        case C_DT: tok.type_datatype = (enum category_datatype)type; break;
+        case C_LT: tok.type_literal =  (enum category_literal)type; break;
+        case C_MD: tok.type_modifier = (enum category_modifier)type; break;
     }
 
 #ifdef DEBUG
